f913d69daadb8e2c459ae5bdcba912c51c63cba1
ElianaDG <58051386+ElianaDG@users.noreply.github.com>
2021-04-25 17:09:30 -0400

    OpenWeatherMapFeed and test

--- src/main/java/gewirtz/openweathermap/OpenWeatherMapFeed.java
+++ src/main/java/gewirtz/openweathermap/OpenWeatherMapFeed.java
@@ -1,5 +1,23 @@
 package gewirtz.openweathermap;
 
+import java.util.List;
+
 public class OpenWeatherMapFeed {
-    
+    public Main main;
+
+    public static class Main {
+        double temp;
+        double feels_like;
+        double temp_min;
+        double temp_max;
+        double pressure;
+        double humidity;
+
+        public double getTemp(){ return temp;}
+        public double getFeelsLike(){ return feels_like;}
+        public double getTempMin(){ return temp_min;}
+        public double getTempMax(){ return temp_max;}
+        public double getPressure(){ return pressure;}
+        public double getHumidity(){ return humidity;}
+    }
 }
--- src/test/java/OpenWeatherMapServiceTest.java
+++ src/test/java/OpenWeatherMapServiceTest.java
@@ -1,2 +1,35 @@
+import gewirtz.openweathermap.OpenWeatherMapFeed;
+import gewirtz.openweathermap.OpenWeatherMapService;
+import io.reactivex.rxjava3.core.Single;
+import org.junit.Test;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static org.junit.Assert.*;
+
 public class OpenWeatherMapServiceTest {
+
+    @Test
+    public void getCurrentWeather(){
+        //given
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://openweathermap.org")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .build();
+
+        OpenWeatherMapService service = retrofit.create(OpenWeatherMapService.class);
+
+        //when
+        Single<OpenWeatherMapFeed> single = service.getCurrentWeather("London");
+        OpenWeatherMapFeed feed = single.blockingGet();
+
+        //then
+        assertNotNull(feed);
+        assertNotNull(feed.main);
+        assertNotNull(feed.main.getTemp());
+
+
+    }
 }
